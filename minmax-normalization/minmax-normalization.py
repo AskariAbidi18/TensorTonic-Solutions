33e364ef89098a29b0976e2a0a5bd2f21c9ce23f
Syed Mohammad Askari Abidi <askari.abidi.2005@gmail.com>
2026-06-22 12:37:20 +0530

    Solve minmax-normalization

---
+++ minmax-normalization/minmax-normalization.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def minmax_scale(X, axis=0, eps=1e-12):
+    """
+    Scale X to [0,1]. If 2D and axis=0 (default), scale per column.
+    Return np.ndarray (float).
+    """
+    # Write code here
+    X = np.array(X)
+
+    X_min = np.min(X, axis = axis, keepdims = True)
+    X_max = np.max(X, axis = axis, keepdims = True)
+
+    X_scaled = (X - X_min) / np.maximum((X_max - X_min), eps)
+
+    return X_scaled
+    
